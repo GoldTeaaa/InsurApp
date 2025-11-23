b43d7f0396dcad06c8a74f5eb3e87af33e59af62
GoldTeaaa <handayariawan@gmail.com>
2025-11-23 08:52:43 +0700

    Add Promise in searchParamsProps

--- src/lib/laporan/laporan-aging-premi/types.ts
+++ src/lib/laporan/laporan-aging-premi/types.ts
@@ -1,13 +1,13 @@
 import { AGING_RANGE } from "@/lib/types";
 import { z } from "zod";
 
-export type searchParamsProps = {
+export type searchParamsProps = Promise<{
     search: string;
     page: string;
     size: string;
     date_from: string;
     date_to: string;
-}
+}>;
 
 const LaporanAgingPremiRowSchema = z.object({
     nomor_polis: z.string(),

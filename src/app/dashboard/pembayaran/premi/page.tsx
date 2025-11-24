753f03c80a6a5fc702d8cf0ba38b2dd7f340ed4c
GoldTeaaa <handayariawan@gmail.com>
2025-11-24 16:28:46 +0700

    Fix to return JSX Element instead of object in dashboard/pembayaran pages

--- src/app/dashboard/pembayaran/premi/page.tsx
+++ src/app/dashboard/pembayaran/premi/page.tsx
@@ -15,10 +15,7 @@ export default async function Page({ searchParams }: {
 
     const parsed = SearchParamsSchema.safeParse(normalized);
     if (!parsed.success) {
-        return {
-            success: false,
-            message: parsed.error.message,
-        };
+        throw new Error(parsed.error.message);
     }
     const params = parsed.data;
     const search = params?.search ?? "";
